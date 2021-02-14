f967aff0824248029fcf0a33d85d417639956e81
konntroll <grigory.koltashev@gmail.com>
2021-02-14 21:26:23 +0000

    Figuring out observables

--- server/server.js
+++ server/server.js
@@ -13,13 +13,23 @@ const io = require('socket.io')(http, {
 
 const port = process.env.PORT || 3000;
 
+let players = [{name: 'Opponent 1', coins: 4},
+               {name: 'Opponent 2', coins: 3},
+               {name: 'Opponent 3', coins: 2},
+               {name: 'Opponent 4', coins: 5}
+              ];
+
 io.on('connection', (socket) => {
 
     console.log('user connected');
 
     socket.on('message', (msg) => {
-      console.log('message: ' + msg);
+      console.log('message: ' + msg.name);
       socket.emit('response', 'message received');
+    });
+
+    socket.on('load', () => {
+      socket.emit('players', players);
     });
 
     socket.on('disconnect', () => {
